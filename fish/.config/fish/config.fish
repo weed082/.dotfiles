307ccc18c374e06377a612a49e5a6f7c78a40b49
weed082 <forsythia3240@gmail.com>
2022-03-05 19:40:33 +0900

    [UPDATE]

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,11 +1,5 @@
 set fish_greeting ""
 set -gx TERM xterm-256color
 
-set -x PATH $PATH /usr/local/go/bin
-set -gx PATH $HOME/.cargo/bin $PATH
-
-# -- theme --
-set -g theme_display_git_default_branch yes
-set -g theme_powerline_fonts no
-set -g theme_nerd_fonts yes
-set -g theme_color_scheme gruvbox
+set -x PATH $PATH /usr/local/go/bin # go 
+set -gx PATH $HOME/.cargo/bin $PATH # rust
