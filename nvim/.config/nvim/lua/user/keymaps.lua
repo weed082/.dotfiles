412a2639a752370b988db690048fe9e863154213
weed082 <forsythia3240@gmail.com>
2022-03-11 00:31:13 +0900

    [UPDATE]

--- nvim/.config/nvim/lua/user/keymaps.lua
+++ nvim/.config/nvim/lua/user/keymaps.lua
@@ -13,12 +13,6 @@ vim.g.maplocalleader = " "
 -- ; => :
 keymap("n", ";", ":", opts)
 
--- Better window navigation
-keymap("n", "<C-h>", "<C-w>h", opts)
-keymap("n", "<C-j>", "<C-w>j", opts)
-keymap("n", "<C-k>", "<C-w>k", opts)
-keymap("n", "<C-l>", "<C-w>l", opts)
-
 -- Resize with arrows
 keymap("n", "<C-Up>", ":resize -2<CR>", opts)
 keymap("n", "<C-Down>", ":resize +2<CR>", opts)
@@ -26,12 +20,12 @@ keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- Navigate buffers
-keymap("n", "<S-l>", ":bn<CR>", opts)
-keymap("n", "<S-h>", ":bp<CR>", opts)
+keymap("n", "<C-l>", ":bn<CR>", opts)
+keymap("n", "<C-h>", ":bp<CR>", opts)
 
 -- Move text up and down
 keymap("n", "<A-j>", "<Esc>:m .+1<CR>==", opts)
-keymap("n", "<A-k>", "<Esc>:m .-3<CR>==", opts)
+keymap("n", "<A-k>", "<Esc>:m .-2<CR>==", opts)
 
 -- Yanke
 keymap("n", "Y", "y$", opts)
@@ -64,8 +58,8 @@ keymap("v", "<", "<gv", opts)
 keymap("v", ">", ">gv", opts)
 
 -- Move text up and down
-keymap("v", "<A-j>", ":m .+1<CR>==gv", opts)
 keymap("v", "<A-k>", ":m .-2<CR>==gv", opts)
+keymap("v", "<A-j>", ":m .+1<CR>==gv", opts)
 keymap("v", "p", '"_dP', opts)
 
 -------------- Visual Block ---------------
