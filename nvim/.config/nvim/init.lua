de2bde5a57f4ee003bc95c2a100189ee68eb7841
weed082 <forsythia3240@gmail.com>
2022-03-14 21:18:06 +0900

    [UPDATE]

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,7 +1,7 @@
+require "user.plugins"
 require "user.impatient"
 require "user.keymaps"
 require "user.options"
-require "user.plugins"
 require "user.colorscheme"
 require "user.lsp"
 require "user.cmp"
