8a660cf2927634463876eb8bc3a2e20306fe821d
weed082 <forsythia3240@gmail.com>
2022-03-19 22:37:45 +0900

    [UPDATE]

--- nvim/.config/nvim/lua/user/colorscheme.lua
+++ nvim/.config/nvim/lua/user/colorscheme.lua
@@ -2,6 +2,6 @@ vim.cmd [[
   let g:everforest_diagnostic_virtual_text='colored'
   let g:everforest_current_word='bold'
   let g:everforest_better_performance=1
-  let g:everforest_background='hard'
+  " let g:everforest_background='hard'
   colorscheme everforest 
 ]]
