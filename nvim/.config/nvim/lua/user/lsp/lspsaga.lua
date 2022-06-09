40a412d2c20a85a0b1815c0c49e1abe51a1147e9
weed082 <forsythia3240@gmail.com>
2022-06-09 10:31:42 +0900

    [DEL] delete unused local variable

--- nvim/.config/nvim/lua/user/lsp/lspsaga.lua
+++ nvim/.config/nvim/lua/user/lsp/lspsaga.lua
@@ -3,7 +3,4 @@ if not status_ok then
   return
 end
 
-local opts = { noremap = true, silent = true }
-local keymap = vim.api.nvim_set_keymap
-
 lspsaga.init_lsp_saga({ code_action_prompt = { virtual_text = false } })
