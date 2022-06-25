f4514a797422c4954b64a60a71db820349e16825
weed082 <forsythia3240@gmail.com>
2022-06-25 11:27:32 +0900

    [UPDATE]

--- nvim/.config/nvim/lua/user/colorscheme.lua
+++ nvim/.config/nvim/lua/user/colorscheme.lua
@@ -1,1 +1,17 @@
+local status, theme = pcall(require, "nightfox")
+if not status then
+  print("nightfox bad status")
+  return
+end
+
+theme.setup({
+  options = {
+    dim_inactive = true,
+    styles = {
+      comments = "italic",
+      keywords = "bold",
+      types = "italic,bold",
+    },
+  },
+})
 vim.cmd([[ colorscheme nightfox ]])
